0f5cf63a00368f4538dfb01abbdcbf573aa3358b
EmielZuurbier <emielzuurbier@outlook.com>
2016-05-14 15:02:46 +0200

    Added spacing in script

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,9 +1,14 @@
 
 Template.login.events({
+	
     'submit form': function(event){
+		
+		// Get values from fields
         event.preventDefault();
         var userVar = event.target.username.value;
         var passwordVar = event.target.password.value;
+		
+		// Login with credentials
         Meteor.loginWithPassword(userVar, passwordVar, function (error) {
 			if (Meteor.user()) {
                 Router.go('/');
@@ -12,29 +17,43 @@ Template.login.events({
 			}
 		});
     },
+	
 	'click .logout': function () {
+		
+		// Logout
 		Meteor.logout();
+		
 	}
+	
 });
 
 Template.login.helpers({
+	
 	User () {
 		var user = Meteor.users.findOne({_id: Meteor.userId()});
 		return user.username;
 	}
-})
+	
+});
 
 Template.register.events({
+	
 	'submit form': function(event) {
+		
+		// Get values from fields
         event.preventDefault();
 		var userVar = event.target.username.value;
         var emailVar = event.target.email.value;
         var passwordVar = event.target.password.value;
+		
+		// Create user
         Accounts.createUser({
             email: emailVar,
 			username: userVar,
             password: passwordVar
         });
+		
+		// Login with usercredentials
 		Meteor.loginWithPassword(userVar, passwordVar, function (error) {
 			if (Meteor.user()) {
                 Router.go('/');
@@ -43,4 +62,5 @@ Template.register.events({
 			}
 		});
     }
-})
+	
+});
